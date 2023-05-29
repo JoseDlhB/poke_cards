5b273fd99504f7a97e6e4542285806e5efae5ea0
Jose DlhB <josehoz.ing@gmail.com>
2023-05-29 00:55:47 -0500

    CSS Cards

--- src/components/Poke.jsx
+++ src/components/Poke.jsx
@@ -1,5 +1,6 @@
 import React , {useState} from 'react'
 import { UseFetchUrl } from './UseFetchUrl'
+import { Cards } from './Cards/Cards'
 
 const Poke = () => {
     const [url, setUrl] = useState('https://pokeapi.co/api/v2/pokemon')
@@ -8,7 +9,17 @@ const Poke = () => {
     loading? console.log('loading..'): console.log(data.results)
 
   return (
-    <div>Poke</div>
+    <div>
+        <h1>CONSUMO DE POKEAPI</h1>
+
+        {
+            loading
+            ?
+            <h1>Loading..</h1>
+            :
+            <Cards results={data.results}/>
+        }
+    </div>
   )
 }
 
